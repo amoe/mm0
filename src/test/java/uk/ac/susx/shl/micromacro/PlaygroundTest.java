019d1a09e1207647637618d972742b24ff9af2bb
David Banks <amoebae@gmail.com>
2019-07-31 16:06:42 +0100

    ws

--- src/test/java/uk/ac/susx/shl/micromacro/PlaygroundTest.java
+++ src/test/java/uk/ac/susx/shl/micromacro/PlaygroundTest.java
@@ -11,26 +11,37 @@ import uk.ac.susx.shl.micromacro.jdbi.DatumStringMapper;
 import uk.ac.susx.tag.method51.core.data.store2.query.Select;
 import uk.ac.susx.tag.method51.core.data.store2.query.SqlQuery;
 import uk.ac.susx.tag.method51.core.data.store2.query.Update;
+import uk.ac.susx.tag.method51.core.meta.Key;
+import uk.ac.susx.tag.method51.core.meta.filters.DatumFilter;
+import uk.ac.susx.tag.method51.core.meta.filters.KeyFilter;
+import uk.ac.susx.tag.method51.core.meta.filters.impl.LabelDecisionFilter;
+import uk.ac.susx.tag.method51.core.meta.filters.logic.LogicParser;
+import uk.ac.susx.tag.method51.core.meta.types.RuntimeType;
+import uk.ac.susx.tag.method51.twitter.LabelDecision;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlaygroundTest {
     private static Logger LOG = LoggerFactory.getLogger(PlaygroundTest.class);
 
     @Test
-    public void foo() throws Exception {
+    public void runSelectQuery() throws Exception {
         LOG.info("sanity check");
-
-        SqlQuery query = new Select("sampled").limit(10);
         PGSimpleDataSource ds = new PGSimpleDataSource();
-
         ds.setDatabaseName("micromacro");
         ds.setUser("micromacro");
         ds.setPassword("xyzzy");
-
         Jdbi j = Jdbi.create(ds);
-
+        Key<LabelDecision> arg1 = Key.of("ob.place/classifier-1750-1825-pubs", RuntimeType.of(LabelDecision.class));
+        KeyFilter kf = new LabelDecisionFilter(arg1, "other");
+        Map<String, KeyFilter> literals = new HashMap<>();
+        literals.put("a", kf);
+        LogicParser p = new LogicParser(literals);
+        DatumFilter df = p.parse(null, "a");
+        SqlQuery query = new Select("sampled").where(df);
         BaseDAO<String, SqlQuery> d = new BaseDAO<>(j, new DatumStringMapper());
         List<String> results = d.list(query);
         LOG.info("results are {}", results.size());
